01adf1762820e0f9bf30d56f036c39799fdc03ba
Ryan Hickey <rhickey94@gmail.com>
2022-05-26 22:15:45 -0400

    first pass

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+const pokemonSearchTerm = ref("");
+</script>
+
+<template>
+  <div class="pokemon-search-container">
+    <input
+      class="pokemon-search"
+      v-model="pokemonSearchTerm"
+      placeholder="Search for a pokemon"
+    />
+  </div>
+</template>
+
+<style>
+.pokemon-search-container {
+  display: flex;
+  justify-content: center;
+}
+
+.pokemon-search {
+  border-radius: 15px;
+  padding: 5px;
+  padding-left: 15px;
+}
+</style>
--- src/views/PokemonHome.vue
+++ src/views/PokemonHome.vue
@@ -1,9 +1,11 @@
 <script setup>
-import SearchContainer from "../components/SearchContainer.vue";
+import SearchContainer from "@/components/SearchContainer.vue";
+import SearchBar from "@/components/SearchBar.vue";
 </script>
 
 <template>
   <div id="home-container">
+    <SearchBar />
     <SearchContainer />
   </div>
 </template>
